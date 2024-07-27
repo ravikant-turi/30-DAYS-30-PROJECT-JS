84eeac68bbce3b23b6d420e9c98cc161aeebc376
ravikant-turi <turiravikant@gamil.com>
2024-07-27 12:37:28 +0530

    commit all

--- 20-CountDownWebsite/app.js
+++ 20-CountDownWebsite/app.js
@@ -3,7 +3,11 @@ const days=document.querySelector('#days')
 
 
 const quote=document.querySelector('.quote')
-// const api='http://zenquotes.io/api/today'
+
+
+const loader=document.querySelector('.loader')
+
+const countdown=document.querySelector('.countdown')
 
 let today=new Date()
 
@@ -19,33 +23,38 @@ let milliSecondInADay=24*60*60*1000
 
 let dayLeft=Math.floor( (nextYear-today)/milliSecondInADay )
 
-
-
 days.innerText=dayLeft
-const url = 'https://quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com/quote?token=ipworld.info';
-const options = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': '4e8f4e97fbmshef684590adf91e2p13d77fjsn771eb52a4d0a',
-		'x-rapidapi-host': 'quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com'
-	}
-};
 
-// Define an async function to use await inside
-// async function fetchQuote() {
-// 	try {
-// 		const response = await fetch(url, options); // Fetch data from API
-// 		const result = await response.json(); // Parse response as text
-// 		console.log(result); // Log the result
-       
-//         quote.innerText=result.text;
-        
-// 	} catch (error) {
-// 		console.error(error); // Log any errors
-// 	}
-// }
 
-// Call the async function to execute the fetch operation
-// fetchQuote();
+fetch('https://api.quotable.io/random')
+
+  .then(res => res.json())
+
+  .then(data => {
+   loader.classList.add('hidden')
+   countdown.classList.remove('hidden')
+    quote.innerText=data.content
+    console.log(data.content)})
+
+  .catch(error => console.error('Error:', error));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
